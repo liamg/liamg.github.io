78e164e150fabaab3f28b8116876c15996e61599
Liam Galvin <liam@liam-galvin.co.uk>
2020-11-10 10:20:50 +0000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var links = document.links;
     for (var i = 0; i < links.length; i++) {
-        if(links[i].href && links[i].href.includes("//")) {
+        if(links[i].href && links[i].href.indexOf(document.baseURI) != 0) {
             links[i].target = "_blank";
             links[i].rel = "noopener"
         }
